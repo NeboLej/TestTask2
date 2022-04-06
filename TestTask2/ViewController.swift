958d7d34444721d6e84481045db14e86eed7cec0
Nebo <kindersmail08@gmail.com>
2022-04-06 14:28:39 +0300

    init

---
+++ TestTask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask2
+//
+//  Created by Nebo on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
